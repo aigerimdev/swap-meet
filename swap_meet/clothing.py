c8fbed37799b466eebdc669b5de7f6f67a7d0219
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-04-01 14:42:14 -0700

    define Clothing class and implement get_category and stringify method, all tests pass

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,15 @@
+import uuid
 class Clothing:
-    pass
+    def __init__(self, id = None, fabric = None):
+        if id is None:
+            self.id = uuid.uuid4().int 
+        else:
+            self.id = id
+        if fabric is None:
+           self.fabric = "Unknown"
+        else:
+            self.fabric = fabric
+    def get_category(self):
+        return "Clothing"    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
