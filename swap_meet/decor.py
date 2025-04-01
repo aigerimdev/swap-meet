baa164c86ef82ecf6d8dc9a2a802d79da26f7a06
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-04-01 14:55:21 -0700

    iplements Decor class and get_category and strigify methods, all tests pass

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,23 @@
+import uuid
 class Decor:
-    pass
+    def __init__(self, id = None, width = None, length=None):
+        if id is None:
+            self.id = uuid.uuid4().int 
+        else:
+            self.id = id
+            
+        if width is None:
+            self.width = 0
+        else:
+            self.width = width
+        
+        if length is None:
+            self.length = 0    
+        else:
+            self.length = length
+        
+    def get_category(self):
+        return "Decor"
+    
+    def __str__(self):
+        return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
